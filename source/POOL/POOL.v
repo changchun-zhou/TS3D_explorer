d2ea6056ede11a35396e05897efef456340657ae
zhouchch3 <2835079913@qq.com>
2020-02-22 11:48:21 +0800

    finish TS3D next is Pooling

--- source/POOL/POOL.v
+++ source/POOL/POOL.v
@@ -12,8 +12,12 @@
 module POOL(
     input                   clk     ,
     input                   rst_n   ,
-    input                         ,
-    output                         
+    input [ 1+`POOL_KERNEL_WIDTH -1 : 0]                  CFG_POOL      ,
+ //   input 						PEBPOOL_ValRow,
+    output  [ 1			        -1 : 0] POOLPEL_EnRd,// 4 bit ID of PEB
+    output  [ `C_LOG_2(`LENPSUM * `LENPSUM)         -1 : 0] POOLPEL_AddrRd,
+    input [ PSUM_WIDTH * `BLOCK_DEPTH      -1 : 0] PELPOOL_Dat,
+               
 );
 //=====================================================================================================================
 // Constant Definition :
@@ -34,15 +38,43 @@ module POOL(
 //=====================================================================================================================
 // Logic Design :
 //=====================================================================================================================
-
-
-
+always @ (posedge clk or negedge rst_n) begin
+	if(~rst_n) begin
+		POOLPEB_EnRd <= 0;
+	end else if( PEBPOOL_ValRow && pool_write_ready) begin
+		POOLPEB_EnRd <= 1;
+ 	end
+end
 
 
 
 //=====================================================================================================================
 // Sub-Module :
 //=====================================================================================================================
+// ==================================================================
+
+  assign pool_write_req =  POOLPEB_EnRd  ;
+  assign pool_write_data = lrn_enable_local ? norm_out : pe_write_data;
+  assign pool_read_ready = 1'b1;
+  pooling #(
+    // INPUT PARAMETERS
+    .OP_WIDTH                 ( `DATA_WIDTH                 ),
+    .NUM_PE                   ( `LENPSUM                   )
+  ) pool_DUT (
+    // PORTS
+    .clk                      ( clk                      ),
+    .reset                    ( !rst_n                    ),
+    .ready                    (                          ),//output
+    .cfg                      ( CFG_POOL               ),
+    .ctrl                     ( pool_ctrl_d              ),
+    .read_data                ( pool_read_data           ),//output
+    .read_req                 ( pool_read_req            ),//
+    .read_ready               ( pool_read_ready          ),//output
+    .write_data               ( PELPOOL_Dat          ),
+    .write_req                ( pool_write_req           ),push
+    .write_ready              ( pool_write_ready         )//output  not full
+  );
+// ==================================================================
 
 
 endmodule
